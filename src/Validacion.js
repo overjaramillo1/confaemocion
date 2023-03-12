e7df0b6e1cf6f04c4595c773dfa5a3e4f262b924
overjaramillo1 <jaramil161@gmail.com>
2023-03-12 18:07:28 -0500

    Mensaje de confirmació2n

--- src/Validacion.js
+++ src/Validacion.js
@@ -13,7 +13,7 @@ export default function Validacion() {
     var strImage = imageSrc.replace(/^data:image\/[a-z]+;base64,/, "");
     setimgTake(imageSrc);
 
-
+    
     var token = localStorage.getItem("tk");
     var requestOptions = {
       method: "POST",
